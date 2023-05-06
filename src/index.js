8af4d77fb047ef01acfa6c00dcdd70fe0d16f20c
igorfa17 <igorfa17@gmail.com>
2023-05-05 22:58:03 -0300

    rotas de criação e login criadas

--- src/index.js
+++ src/index.js
@@ -8,3 +8,42 @@ app.get('/', (request, response) => {
 return response.json('OK');
 });
 app.listen(8080, () => console.log("Servidor iniciado"));
+
+const users = [];
+
+app.post('/signup', (request, response) => {
+  const { name, email, password } = request.body;
+
+  if (!name || !email || !password) {
+    return response.status(400).send({ error: 'Nome, email e senha são obrigatórios' });
+  }
+
+  const userExists = users.some(user => user.email === email);
+
+  if (userExists) {
+    return response.status(400).send({ error: 'Um usuário com este email já foi cadastrado.' });
+  }
+
+  const user = { id: users.length + 1, name, email, password };
+
+  users.push(user);
+
+  return response.status(201).send({ user });
+});
+
+app.post('/login', (request, response) => {
+    const { email, password } = request.body;
+  
+    if (!email || !password) {
+      return response.status(400).send({ error: 'Email e senha são obrigatórios.' });
+    }
+  
+    const user = users.find(user => user.email === email && user.password === password);
+  
+    if (!user) {
+      return response.status(401).send({ error: 'Email ou senha incorretos.' });
+    }
+  
+    return response.send({ user });
+  });
+  
